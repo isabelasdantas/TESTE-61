3c0ea7480e4067fd1404fa5b769164a197d06eea
Isa Dantas <157421767+isabelasdantas@users.noreply.github.com>
2024-02-03 12:28:31 -0300

    Add files via upload

---
+++ TESTE61.py
@@ -0,0 +1,9 @@
+primeiro_termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+termo = primeiro_termo
+cont = 1
+while cont <= 10:
+    print('{} --> '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('Fim')
